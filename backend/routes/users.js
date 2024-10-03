e342b6be0db164811a4083b8b7c1d62c342ec741
Matheus Pereira Martins <matheuspereiramartins1993@gmail.com>
2024-10-03 12:41:12 -0300

    perfect front-end

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -43,36 +43,64 @@ router.get('/:userId', async (req, res) => {
 router.post('/', upload.single('photo'), async (req, res) => {
     const { name, email, password } = req.body;
 
-    const newUser = new User({
-        name,
-        email,
-        password,
-        photo: req.file ? req.file.path : 'uploads/user_photo.png',
-        score: 0,
-        courses: [
-            {
-                course: "cockpit",
-                progress: 0,
-                lessons: [
-                    { title: "Prejuizos", completed: false },
-                    { title: "Milhas", completed: false }
-                ]
-            },
-            // Adicione outros cursos aqui...
-        ]
-    });
-
-    console.log("Courses before save:", newUser.courses);
-
     try {
+        const newUser = new User({
+            name,
+            email,
+            password,
+            photo: req.file ? req.file.path : 'uploads/user_photo.png', // Se uma foto não foi enviada, usa a imagem padrão
+            score: 0, // Pontuação inicial
+            courses: [
+                {
+                    "course": "cockpit",
+                    "progress": 0,
+                    "lessons": [
+                        {
+                            "title": "Prejuizos",
+                            "completed": false,
+
+                        },
+                        {
+                            "title": "Milhas",
+                            "completed": false,
+
+                        }
+                    ]
+                },
+                {
+                    "course": "milhas",
+                    "progress": 0,
+                    "lessons": []
+                },
+                {
+                    "course": "historia",
+                    "progress": 0,
+                    "lessons": []
+                },
+                {
+                    "course": "nocao",
+                    "progress": 0,
+                    "lessons": []
+                },
+                {
+                    "course": "ofertantes",
+                    "progress": 0,
+                    "lessons": []
+                },
+                {
+                    "course": "tour_virtual",
+                    "progress": 0,
+                    "lessons": []
+                }
+            ]
+        });
+
         await newUser.save();
-        res.status(201).json(newUser);
+        res.status(201).json(newUser); // Retorna o usuário criado
     } catch (error) {
-        console.error('Error saving user:', error);
         res.status(500).json({ message: 'Erro ao criar usuário', error: error.message });
     }
 });
-
 
 // Rota para atualizar um usuário
 router.put('/update/:id', upload.single('photo'), async (req, res) => {
