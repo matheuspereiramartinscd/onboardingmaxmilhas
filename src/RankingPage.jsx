40babebcf75c4c1f45a6c5797940584ea8c519b6
Matheus Pereira Martins <matheus.martins@maxmilhas.local>
2024-10-10 13:30:32 -0300

    apenas emails maxmilhas

--- src/RankingPage.jsx
+++ src/RankingPage.jsx
@@ -47,8 +47,8 @@ const RankingPage = () => {
                     Array.from({ length: 6 }).map((_, index) => (
                         <div key={index} className={styles.collaboratorCard}>
                             <div className={styles.imageContainer}>
-                                <img src={gear} alt="Placeholder" className={styles.userPhoto} />
-                                <span className={styles.loadingText}>Carregando....</span>
+                            <span className={styles.loadingText}>Carregando.... &nbsp;&nbsp;</span>
+                                <span className={styles.loadingText}>Carregando....&nbsp;&nbsp;</span>
                             </div>
                             <div className={styles.userInfo}>
                                 <span className={styles.userName}>Carregando...</span>
